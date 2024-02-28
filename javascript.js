00f2431a9bf920765f88dc1bd80fb2d14eaa9fca
Cecilia <ravacecilia1@gmail.com>
2024-02-28 14:03:02 -0300

    feat: Update 'javascript.js' to add calculator functionality

--- javascript.js
+++ javascript.js
@@ -1,105 +1,131 @@
-//LOGIC
-function operate(firstNumber, secondNumber, operator){
-    let result;
-    switch (operator){
-        case '+':
-            result = firstNumber + secondNumber;
-            break;
-        case '-':
-            result = firstNumber - secondNumber;
-            break;
-        case '*':
-            result = firstNumber * secondNumber;
-            break;
-        case '/':
-            result = firstNumber / secondNumber;
-            break;
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
     }
-    return result;
+
+    clear(){
+        this.previousOperand = '';
+        this.currentOperand = '';
+        this.operation = undefined;
+    }
+
+    delete(){
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    }
+
+    appendNumber(number){
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+
+    chooseOperation(operation){
+        if (this.currentOperand === '') return;
+        if (this.previousOperand !== '') {
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+    }
+
+    compute(){
+        let computation;
+        const previous = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(previous) || isNaN(current)) return
+        switch (this.operation){
+            case '+':
+                computation = previous + current;
+                break;
+            case '-':
+                computation = previous - current;
+                break;
+            case '*':
+                computation = previous * current;
+                break;
+            case '/':
+                computation = previous / current;
+                break;
+            default: 
+                return 
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
+    }
+
+
+    getDisplayNumber(number){
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split('.')[0]);
+        const decimalDigits = stringNumber.split('.')[1];
+        let integerDisplay;
+        if (isNaN(integerDigits)) {
+            integerDisplay = '';
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0})
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`;
+        } else {
+            return integerDisplay;
+        }        
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText =
+        this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) {
+        this.previousOperandTextElement.innerText =
+            `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+        this.previousOperandTextElement.innerText = ''
+        }
+    }
 }
-/*
-let firstNumber = parseFloat(prompt("Input the first number",0));
-let secondNumber = parseFloat(prompt("Input the second number",0));
-let operator = prompt("Input the operator. It can be '+', '-', '*' or '/'", "")
-let result = operate(firstNumber, secondNumber, operator);
-console.log(result);
-*/
 
 //DOM variables. To control this elements by Js
-const seven_div = document.getElementById('seven');
-const eight_div = document.getElementById('eight');
-const nine_div = document.getElementById('nine');
-const divide_div = document.getElementById('divide');
-
-const four_div = document.getElementById('four');
-const five_div = document.getElementById('five');
-const six_div = document.getElementById('six');
-const multiply_div = document.getElementById('multiply');
-
-const one_div = document.getElementById('one');
-const two_div = document.getElementById('two');
-const three_div = document.getElementById('three');
-const subtract_div = document.getElementById('subtract');
-
-const happy_div = document.getElementById('happy');
-const zero_div = document.getElementById('zero');
-const add_div = document.getElementById('add');
-const equal_div = document.getElementById('equal');
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+const equalsButton = document.querySelector('[data-equals]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
 
 
-//MAIN
-function main() {
-    seven_div.addEventListener('click', function() {
-        operate('seven');
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
     })
-    eight_div.addEventListener('click', function() {
-        operate('eight');
-    })
-    nine_div.addEventListener('click', function() {
-        operate('nine');
-    })
-    divide_div.addEventListener('click', function() {
-        operate('divide');
-    })
+});
 
-    four_div.addEventListener('click', function() {
-        operate('four');
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
     })
-    five_div.addEventListener('click', function() {
-        operate('five');
-    })
-    six_div.addEventListener('click', function() {
-        operate('six');
-    })
-    multiply_div.addEventListener('click', function() {
-        operate('multiply');
-    })
+})
 
-    one_div.addEventListener('click', function() {
-        operate('one');
-    })
-    two_div.addEventListener('click', function() {
-        operate('two');
-    })
-    three_div.addEventListener('click', function() {
-        operate('three');
-    })
-    subtract_div.addEventListener('click', function() {
-        operate('subtract');
-    })
+equalsButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+})
 
-    happy_div.addEventListener('click', function() {
-        operate('happy');
-    })
-    zero_div.addEventListener('click', function() {
-        operate('zero');
-    })
-    add_div.addEventListener('click', function() {
-        operate('add');
-    })
-    equal_div.addEventListener('click', function() {
-        operate('equal');
-    })
-}
-main();
+allClearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
+})
+
+
+
 
